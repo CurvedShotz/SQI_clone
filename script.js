305a65161b93e3ba5a3d30f0fb1e841dd6e9e9e4
CurvedShotz <124447673+CurvedShotz@users.noreply.github.com>
2024-10-06 19:47:39 +0100

    even more changes

---
+++ script.js
@@ -0,0 +1,17 @@
+document.addEventListener('DOMContentLoaded', function() {
+  
+  var dropdowns = document.querySelectorAll('.nav-item.dropdown');
+
+
+  dropdowns.forEach(function(dropdown) {
+    dropdown.addEventListener('mouseover', function() {
+      var dropdownMenu = this.querySelector('.dropdown-menu');
+      dropdownMenu.classList.add('show'); 
+    });
+
+    dropdown.addEventListener('mouseleave', function() {
+      var dropdownMenu = this.querySelector('.dropdown-menu');
+      dropdownMenu.classList.remove('show');  
+    });
+  });
+});
